57876ea355217e89e8c236e0203c9aa56f7b987d
root <root@develop.g2.com>
2016-08-13 01:37:52 +0800

    second commit

--- learnShellByDemo.sh
+++ learnShellByDemo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#LANG=zh_CN.utf8
 
 DelayCount()
 {
@@ -9,4 +10,27 @@ DelayCount()
 			done
 }
 
-DelayCount 30
+CheckTargetExist()
+{
+	ready_status=""
+	checkCount=0
+	echo "检测 $1"
+	while [ "$ready_status" = "" ]
+	do
+		sleep 1
+		ready_status=`ls -l $1 2>/dev/null`
+		((checkCount=$checkCount+1))
+		echo $checkCount"/30..."
+		if [ "$checkCount" == "30" ]
+		then
+			echo "检测时间过长,启动出错，终止进程"
+			exit 1
+		fi		
+	done
+	echo "删除 $1"
+	rm -f $1
+}
+
+#DelayCount 30
+
+CheckTargetExist a.txt
