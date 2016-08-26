392b323f97dc1e96a371485b74adae894f38b53d
U-USER7\Administrator <Administrator@USER7.(none)>
2016-08-26 17:06:40 +0800

    client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('127.0.0.1',9999))
+print s.recv(1024)
+for data in ['edward',"hahaha"]:
+    s.send(data)
+    print s.recv(1024)
+s.send('exit')
+s.close()
